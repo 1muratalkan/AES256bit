2044d97f4c36dbde947bbeb5384ce01d5a06a4b3
MURAT ALKAN <108190826+1muratalkan@users.noreply.github.com>
2024-05-09 11:32:37 +0300

    Update tb_aes_encrypt_256.vhd

--- aes_encrypt_256/Testbench/Tb _and_Text_IO_files/tb_aes_encrypt_256.vhd
+++ aes_encrypt_256/Testbench/Tb _and_Text_IO_files/tb_aes_encrypt_256.vhd
@@ -32,15 +32,12 @@ end component;
 
 signal  clk_i                : std_logic :='1';
 signal  reset_i              : std_logic :='1';
---
 signal  plain_text_i         : std_logic_vector(127 downto 0);
 signal  plain_text_vld_i     : std_logic :='0';
 signal  plain_text_ready_o   : std_logic;
---
 signal  key_i                : std_logic_vector(127 downto 0);
 signal  key_vld_i            : std_logic:='0';
 signal  key_ready_o          : std_logic;
--- 
 signal  cipher_text_o        : std_logic_vector(127 downto 0);
 signal  cipher_text_vld_o    : std_logic;
 signal  cipher_text_ready_i  : std_logic:='0';
@@ -54,27 +51,24 @@ constant ECBVarKey256_FILE_RD   :string  := "/home/murat/Documents/Uygulamalar/v
 constant ECBVarTxt256_FILE_RD   :string  := "/home/murat/Documents/Uygulamalar/vivado/my_projects/aes_encrypt_256/aes_encrypt_256/aes_encrypt_256.srcs/sim_1/new/ECBVarTxt256.txt";
 constant ECBKeySbox256_FILE_RD  :string  := "/home/murat/Documents/Uygulamalar/vivado/my_projects/aes_encrypt_256/aes_encrypt_256/aes_encrypt_256.srcs/sim_1/new/ECBKeySbox256.txt";
 
-
- 
-
 begin
 
     aes_encrypt_256_port : aes_encrypt_256
     port map (
-                  clk_i                =>   clk_i                ,
-                  reset_i              =>   reset_i              ,
-                  --                 
-                  plain_text_i         =>   plain_text_i         ,
-                  plain_text_vld_i     =>   plain_text_vld_i     ,
-                  plain_text_ready_o   =>   plain_text_ready_o   ,
-                  --                          
-                  key_i                =>   key_i                ,
-                  key_vld_i            =>   key_vld_i            ,
-                  key_ready_o          =>   key_ready_o          ,
-                  --                  
-                  cipher_text_o        =>   cipher_text_o        ,
-                  cipher_text_vld_o    =>   cipher_text_vld_o    ,
-                  cipher_text_ready_i  =>   cipher_text_ready_i                
+        clk_i                =>   clk_i                ,
+        reset_i              =>   reset_i              ,
+        --                 
+        plain_text_i         =>   plain_text_i         ,
+        plain_text_vld_i     =>   plain_text_vld_i     ,
+        plain_text_ready_o   =>   plain_text_ready_o   ,
+        --                          
+        key_i                =>   key_i                ,
+        key_vld_i            =>   key_vld_i            ,
+        key_ready_o          =>   key_ready_o          ,
+        --                  
+        cipher_text_o        =>   cipher_text_o        ,
+        cipher_text_vld_o    =>   cipher_text_vld_o    ,
+        cipher_text_ready_i  =>   cipher_text_ready_i                
     );
 
     -- clock process definitions
@@ -86,71 +80,29 @@ begin
         wait for clk_period/2;
     end process clk_process;
 
-
     test_process : process
     
-        variable input_line                 : line;
-        variable PLAINTEXT                  : std_logic_vector(127 downto 0);
-        variable KEY                        : std_logic_vector(255 downto 0);
-        variable CIPHERTEXT                 : std_logic_vector(127 downto 0);
+        procedure aes_procedure ( constant VECTOR_FILE : string;
+                                  constant VECTOR_TYPE : string) is
         
-        file input_file 			        : text open read_mode is TEST_FILE_RD;
-        file VEC_ECBGFSbox256_FILE_RD	    : text open read_mode is ECBGFSbox256_FILE_RD;
-        file VEC_ECBVarKey256_FILE_RD	    : text open read_mode is ECBVarKey256_FILE_RD;
-        file VEC_ECBVarTxt256_FILE_RD	    : text open read_mode is ECBVarTxt256_FILE_RD;
-        file VEC_ECBKeySbox256_FILE_RD	    : text open read_mode is ECBKeySbox256_FILE_RD;
-
-
-        variable    COUNT                   : integer;                
-        variable    str_count               : string(1 to 8);             
-        variable    str_key                 : string(1 to 6);             
-        variable    str_plain               : string(1 to 12);             
-        variable    str_cipher              : string(1 to 13);                                 
-   
-    procedure test_procedure is
-    begin
-            plain_text_i <= PLAINTEXT;
-            plain_text_vld_i <= '1';  
-            wait for clk_period;
-            plain_text_i <= (others => '0');
-            plain_text_vld_i <= '0';
-            wait for clk_period;
-            key_vld_i <= '1';
-            key_i <= KEY(255 downto 128);--  
-            wait for clk_period;
-            key_i <= KEY(127 downto 0);--
-            key_vld_i <= '1';
-            wait for clk_period;
-            key_i <= (others => '0');
-            key_vld_i <= '0';
-            wait for clk_period*15;     
-            cipher_text_ready_i <= '1';
-            wait for clk_period;
-            wait for clk_period;
-            cipher_text_ready_i <= '0'; 
-            wait for clk_period;
-
-    end procedure;
-
-   
-   
-   
-   
-   
-    begin
-
-
-        reset_i <= '1';
-        wait for clk_period;
-        reset_i <= '0';
-        wait for clk_period;
-        --wait for clk_period/2;
-
-        while not endfile(input_file) loop
+        variable input_line   : line;
+        variable COUNT        : integer;
+        variable str_count    : string(1 to 8);             
+        variable str_key      : string(1 to 6);             
+        variable str_plain    : string(1 to 12);             
+        variable str_cipher   : string(1 to 13); 
+        variable PLAINTEXT    : std_logic_vector(127 downto 0);
+        variable KEY          : std_logic_vector(255 downto 0);
+        variable CIPHERTEXT   : std_logic_vector(127 downto 0);     
         
-            -- Read a row from the text file
-            readline(input_file, input_line);
-           
+        file input_file : text open read_mode is VECTOR_FILE;
+        
+        begin
+        
+            while not endfile(input_file) loop
+            
+                readline(input_file, input_line);-- Read a row from the text file
+              
                 if input_line.all'length = 0 or input_line.all(1) = '#' then
                          next;
                 end if;
@@ -161,8 +113,7 @@ begin
                 elsif input_line.all(1 to 9) = "[DECRYPT]" then 
                          exit;
                 end if;
-                
-                  
+
                 if input_line(1 to 8) = "COUNT = " then
                          read(input_line,str_count);
                          read(input_line,COUNT);
@@ -187,240 +138,62 @@ begin
                          readline(input_file, input_line);        
                  end if;
                  
-            test_procedure;
-
-            if cipher_text_o = CIPHERTEXT then
-                report "Test_vectors file " & integer'image(COUNT) & ": SUCCESSFUL";
-            else    
-                report "Test_vectors file " & integer'image(COUNT) & ": FAIL";
-            end if;
+                --  
+                wait for clk_period; wait for clk_period;wait for clk_period;wait for clk_period;
+                plain_text_i <= PLAINTEXT; 
+                plain_text_vld_i <= '1';  
+                wait for clk_period;
+                plain_text_i <= (others => '0');
+                plain_text_vld_i <= '0';
+                wait for clk_period;
+                wait for clk_period;wait for clk_period;wait for clk_period; wait for clk_period;wait for clk_period;wait for clk_period;
+                key_vld_i <= '1';
+                key_i <= KEY(255 downto 128);--  
+                wait for clk_period;
+                key_i <= KEY(127 downto 0);--
+                plain_text_i <= PLAINTEXT;
+                wait for clk_period;
+                key_i <= (others => '0');
+                key_vld_i <= '0';
+                key_i <= KEY(127 downto 0);--
+                wait for clk_period*20;     
+                cipher_text_ready_i <= '1';
+                wait for clk_period;
+                wait for clk_period;wait for clk_period; wait for clk_period;wait for clk_period;wait for clk_period;
+                cipher_text_ready_i <= '0'; 
+                --
             
-           
-        end loop;
-
+               if cipher_text_o = CIPHERTEXT then
+                   report VECTOR_TYPE & "File " & integer'image(COUNT) & ": SUCCESSFUL";
+               else    
+                   report VECTOR_TYPE & "File " & integer'image(COUNT) & ": FAIL";
+               end if;
             
-        while not endfile(VEC_ECBGFSbox256_FILE_RD) loop
-        
-            -- Read a row from the text file
-            readline(VEC_ECBGFSbox256_FILE_RD, input_line);
-           
-                if input_line.all'length = 0 or input_line.all(1) = '#' then
-                    next;
-                end if;
-                
-                
-                if input_line.all(1 to 9) = "[ENCRYPT]" then
-                           next;
-                elsif input_line.all(1 to 9) = "[DECRYPT]" then 
-                        exit;
-                end if;
-                
-                  
-                if input_line(1 to 8) = "COUNT = " then
-                         read(input_line,str_count);
-                         read(input_line,COUNT);
-                         readline(VEC_ECBGFSbox256_FILE_RD, input_line);
-                         
-                end if;      
-                   
-                if input_line.all(1 to 6) = "KEY = " then
-                         read(input_line,str_key);
-                         hread(input_line,KEY);
-                         readline(VEC_ECBGFSbox256_FILE_RD, input_line);
-                end if;     
-                
-                if input_line.all(1 to 12) = "PLAINTEXT = " then
-                         read(input_line,str_plain);
-                         hread(input_line,PLAINTEXT);
-                         readline(VEC_ECBGFSbox256_FILE_RD, input_line);
-                 end if;
-                   
-                 if input_line.all(1 to 13) = "CIPHERTEXT = " then
-                         read(input_line,str_cipher);
-                         hread(input_line,CIPHERTEXT);
-                         readline(VEC_ECBGFSbox256_FILE_RD, input_line);
-                               
-                 end if;         
-                
-                test_procedure;
-            
-            if cipher_text_o = CIPHERTEXT then
-                report "ECBGFSbox256 file " & integer'image(COUNT) & ": SUCCESSFUL";
-            else    
-                report "ECBGFSbox256 file " & integer'image(COUNT) & ": FAIL";
-            end if;
-
             end loop;
-
-        while not endfile(VEC_ECBVarKey256_FILE_RD) loop
+         
+            file_close(input_file);
+               
+        end procedure aes_procedure;
         
-             -- Read a row from the text file
-            readline(VEC_ECBVarKey256_FILE_RD, input_line);
-           
-                if input_line.all'length = 0 or input_line.all(1) = '#' then
-                    next;
-                end if;
+         
+    begin
                 
-                
-                if input_line.all(1 to 9) = "[ENCRYPT]" then
-                           next;
-                elsif input_line.all(1 to 9) = "[DECRYPT]" then 
-                        exit;
-                end if;
-                
-                  
-                if input_line(1 to 8) = "COUNT = " then
-                         read(input_line,str_count);
-                         read(input_line,COUNT);
-                         readline(VEC_ECBVarKey256_FILE_RD, input_line);
-                         
-                end if;      
-                   
-                if input_line.all(1 to 6) = "KEY = " then
-                         read(input_line,str_key);
-                         hread(input_line,KEY);
-                         readline(VEC_ECBVarKey256_FILE_RD, input_line);
-                end if;     
-                
-                if input_line.all(1 to 12) = "PLAINTEXT = " then
-                         read(input_line,str_plain);
-                         hread(input_line,PLAINTEXT);
-                         readline(VEC_ECBVarKey256_FILE_RD, input_line);
-                 end if;
-                   
-                 if input_line.all(1 to 13) = "CIPHERTEXT = " then
-                         read(input_line,str_cipher);
-                         hread(input_line,CIPHERTEXT);
-                         readline(VEC_ECBVarKey256_FILE_RD, input_line);
-                               
-                 end if;
-                
-                test_procedure;
-            
-            if cipher_text_o = CIPHERTEXT then
-                report "ECBVarKey256 file " & integer'image(COUNT) & ": SUCCESSFUL";
-            else    
-                report "ECBVarKey256 file " & integer'image(COUNT) & ": FAIL";
-            end if;
-
-            end loop;    
-            
-            
-        while not endfile(VEC_ECBVarTxt256_FILE_RD) loop
+        reset_i <= '1';
+        wait for clk_period;
+        reset_i <= '0';
+        wait for clk_period;
         
+        aes_procedure(TEST_FILE_RD, "TEST");
+        aes_procedure(ECBGFSbox256_FILE_RD, "ECBGFSbox256");
+        aes_procedure(ECBVarKey256_FILE_RD, "ECBVarKey256");
+        aes_procedure(ECBVarTxt256_FILE_RD, "ECBVarTxt256");
+        aes_procedure(ECBKeySbox256_FILE_RD, "ECBKeySbox256");
         
-             -- Read a row from the text file
-            readline(VEC_ECBVarTxt256_FILE_RD, input_line);
-           
-                if input_line.all'length = 0 or input_line.all(1) = '#' then
-                    next;
-                end if;
-                
-                
-                if input_line.all(1 to 9) = "[ENCRYPT]" then
-                           next;
-                elsif input_line.all(1 to 9) = "[DECRYPT]" then 
-                        exit;
-                end if;
-                
-                  
-                if input_line(1 to 8) = "COUNT = " then
-                         read(input_line,str_count);
-                         read(input_line,COUNT);
-                         readline(VEC_ECBVarTxt256_FILE_RD, input_line);
-                         
-                end if;      
-                   
-                if input_line.all(1 to 6) = "KEY = " then
-                         read(input_line,str_key);
-                         hread(input_line,KEY);
-                         readline(VEC_ECBVarTxt256_FILE_RD, input_line);
-                end if;     
-                
-                if input_line.all(1 to 12) = "PLAINTEXT = " then
-                         read(input_line,str_plain);
-                         hread(input_line,PLAINTEXT);
-                         readline(VEC_ECBVarTxt256_FILE_RD, input_line);
-                 end if;
-                   
-                 if input_line.all(1 to 13) = "CIPHERTEXT = " then
-                         read(input_line,str_cipher);
-                         hread(input_line,CIPHERTEXT);
-                         readline(VEC_ECBVarTxt256_FILE_RD, input_line);
-                               
-                 end if;
+        report "--------------Simulation completed--------------" severity note;
+        assert FALSE report "--------------Testbench completed--------------" severity FAILURE;
+        
+          
+    end process test_process;
 
-            test_procedure;
-            
-            if cipher_text_o = CIPHERTEXT then
-                report "ECBVarTxt256 file " & integer'image(COUNT) & ": SUCCESSFUL";
-            else    
-                report "ECBVarTxt256 file " & integer'image(COUNT) & ": FAIL";
-            end if;
-
-            end loop;    
-            
-        while not endfile(VEC_ECBKeySbox256_FILE_RD) loop
-              -- Read a row from the text file
-            readline(VEC_ECBKeySbox256_FILE_RD, input_line);
-           
-                if input_line.all'length = 0 or input_line.all(1) = '#' then
-                    next;
-                end if;
-                
-                
-                if input_line.all(1 to 9) = "[ENCRYPT]" then
-                           next;
-                elsif input_line.all(1 to 9) = "[DECRYPT]" then 
-                        exit;
-                end if;
-                
-                  
-                if input_line(1 to 8) = "COUNT = " then
-                         read(input_line,str_count);
-                         read(input_line,COUNT);
-                         readline(VEC_ECBKeySbox256_FILE_RD, input_line);
-                         
-                end if;      
-                   
-                if input_line.all(1 to 6) = "KEY = " then
-                         read(input_line,str_key);
-                         hread(input_line,KEY);
-                         readline(VEC_ECBKeySbox256_FILE_RD, input_line);
-                end if;     
-                
-                if input_line.all(1 to 12) = "PLAINTEXT = " then
-                         read(input_line,str_plain);
-                         hread(input_line,PLAINTEXT);
-                         readline(VEC_ECBKeySbox256_FILE_RD, input_line);
-                 end if;
-                   
-                 if input_line.all(1 to 13) = "CIPHERTEXT = " then
-                         read(input_line,str_cipher);
-                         hread(input_line,CIPHERTEXT);
-                         readline(VEC_ECBKeySbox256_FILE_RD, input_line);
-                               
-                 end if;      
-                
-                test_procedure;
-            
-            if cipher_text_o = CIPHERTEXT then
-                report "ECBKeySbox256 file " & integer'image(COUNT) & ": SUCCESSFUL";
-            else    
-                report "ECBKeySbox256 file " & integer'image(COUNT) & ": FAIL";
-            end if;
-
-            end loop;     
-
-           
-           file_close(input_file);
-           file_close(VEC_ECBGFSbox256_FILE_RD);
-           file_close(VEC_ECBVarKey256_FILE_RD);
-           file_close(VEC_ECBVarTxt256_FILE_RD);
-           file_close(VEC_ECBKeySbox256_FILE_RD);
-           
-           report "--------------Simulation completed--------------" severity note;
-           assert FALSE report "--------------Testbench completed--------------" severity FAILURE;
-    end process;
 
 end Behavioral;
